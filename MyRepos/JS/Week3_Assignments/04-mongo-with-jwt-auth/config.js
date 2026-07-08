e938f7bcf64596cd211a9fd166135d2205bd605e
abhijeetat9 <abhijeet.tawde95@gmail.com>
2026-07-08 14:18:59 +0100

    Remove unrelated HTML/JS/React from JobTracker branch

--- MyRepos/JS/Week3_Assignments/04-mongo-with-jwt-auth/config.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-    JWT_SECRET: "abhijit_server"
-}
--- MyRepos/React/react-demos/src/components/MainContent.jsx
+++
@@ -1,8 +0,0 @@
-const MainContent = () => {
-    return <main>
-        <h2>Main Content</h2>
-        <p>Quick brown fox jumps over the fence</p>
-    </main>;
-};
-
-export default MainContent;
